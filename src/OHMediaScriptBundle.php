5c08c07d2d632331aa5af24e19cc5f433b47793e
Justin Hoffman <justin@ohmedia.ca>
2026-04-23 12:38:14 -0600

    settings for src prefixes

--- src/OHMediaScriptBundle.php
+++ src/OHMediaScriptBundle.php
@@ -8,11 +8,26 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class OHMediaScriptBundle extends AbstractBundle
 {
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->rootNode()
+            ->children()
+                ->arrayNode('iframe_src_prefixes')->end()
+                ->arrayNode('script_src_prefixes')->end()
+            ->end()
+        ;
+    }
+
     public function loadExtension(
         array $config,
         ContainerConfigurator $containerConfigurator,
-        ContainerBuilder $containerBuilder
+        ContainerBuilder $containerBuilder,
     ): void {
         $containerConfigurator->import('../config/services.yaml');
+
+        $containerConfigurator->parameters()
+            ->set('oh_media_script.iframe_src_prefixes', $config['iframe_src_prefixes'])
+            ->set('oh_media_script.script_src_prefixes', $config['script_src_prefixes'])
+        ;
     }
 }
